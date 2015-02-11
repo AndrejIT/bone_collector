9c7669637f2717bd0a87663b1a012abd23704e09
AndrejIT <andrejIT@rambler.ru>
2015-02-11 22:32:05 +0200

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,45 @@
+-- Minetest 0.4 mod: bone_collector
+-- Bones can be crafted to clay, sand or coal to motivate players clear the playground.
+-- 
+-- See README.txt for licensing and other information.
+
+minetest.register_craft({
+	output = 'default:clay_lump',
+	recipe = {
+		{"bones:bones", "", ""},
+		{"", "", ""},
+		{"", "", ""},
+	},
+})
+minetest.register_craft({
+	output = 'default:gravel',
+	recipe = {
+		{"", "", ""},
+		{"", "", ""},
+		{"bones:bones", "bones:bones", "bones:bones"},
+	},
+})
+minetest.register_craft({
+	output = 'default:sand',
+	recipe = {
+		{"bones:bones", "", "bones:bones"},
+		{"", "bones:bones", ""},
+		{"bones:bones", "", "bones:bones"},
+	},
+})
+minetest.register_craft({
+	output = 'default:coal_lump',
+	recipe = {
+		{"", "bones:bones", ""},
+		{"bones:bones", "bones:bones", "bones:bones"},
+		{"", "bones:bones", ""},
+	},
+})
+minetest.register_craft({
+	output = 'default:dirt',
+	recipe = {
+		{"bones:bones", "bones:bones", "bones:bones"},
+		{"bones:bones", "bones:bones", "bones:bones"},
+		{"bones:bones", "bones:bones", "bones:bones"},
+	},
+})
